a5f338da200dcf79f5eade1ac316c7f17b42cdaa
fayzillo95 <ovovovlululutvata@gmail.com>
2025-06-29 11:51:38 +0500

    Review

--- src/modules/users/user-reviews/dto/create-user-review.dto.ts
+++ src/modules/users/user-reviews/dto/create-user-review.dto.ts
@@ -1,1 +1,15 @@
-export class CreateUserReviewDto {}
+import { IsNumber, IsString } from "class-validator";
+import { CreateFavoriteDto } from "../../user-favorite/dto/create-favorite.dto";
+import { Transform } from "class-transformer";
+
+export class CreateUserReviewDto extends CreateFavoriteDto{
+    @Transform((e) => typeof e.value !== "number" ? 
+                    isNaN(+e.value) ? false : 
+                    Number(e.value) : e.value
+                )
+    @IsNumber()
+    rating : number
+
+    @IsString()
+    comment : string
+}
