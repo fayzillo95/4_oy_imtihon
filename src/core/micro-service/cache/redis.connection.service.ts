11e31886cf39f332b5fe880751aa237fff5f691a
fayzillo95 <ovovovlululutvata@gmail.com>
2025-07-02 14:42:49 +0500

    redis reassigned

--- src/core/micro-service/cache/redis.connection.service.ts
+++ src/core/micro-service/cache/redis.connection.service.ts
@@ -6,38 +6,37 @@ export class RedisConnectService implements OnModuleInit, OnModuleDestroy {
   private redisClient: RedisClientType;
 
   async onModuleInit() {
-    const isProd = !!process.env.REDIS_URL;
+    const redisUrl = process.env.REDIS_URL || 'redis://127.0.0.1:6379';
 
-    this.redisClient = createClient({ url: isProd ? process.env.REDIS_URL : 'redis://127.0.0.1:6379' });
+    this.redisClient = createClient({ url: redisUrl });
 
-    await this.redisClient.on('error', (err) =>
-      console.error('Redis error:', err),
-    );
+    this.redisClient.on('error', (err) => {
+      console.error('❌ Redis error:', err);
+    });
 
     await this.redisClient.connect();
-    console.log(`Redis connected\n${isProd ? `\n.env.REDIS_URL= : ${process.env.REDIS_URL}` : 'redis://127.0.0.1:6379'}`);
+
+    console.log(`✅ Redis connected: ${redisUrl}`);
   }
 
   async onModuleDestroy() {
     if (this.redisClient) {
       await this.redisClient.quit();
-      const isProd = !!process.env.REDIS_URL;
-
-      console.log(`Redis disconnected\n${isProd ? `\n.env.REDIS_URL= : ${process.env.REDIS_URL}` : '\nredis://127.0.0.1:6379'}`);
+      console.log('🔌 Redis disconnected');
     }
   }
+
   async setItem(key: string, value: object) {
-    const status = await this.redisClient.set(key, JSON.stringify(value),{ EX: 60 * 10 }); // 10 minutes expiration
-    console.log(status);
+    const status = await this.redisClient.set(key, JSON.stringify(value), { EX: 60 * 10 }); // 10 minutes
     return status;
   }
+
   async getValue<T>(key: string): Promise<T | null> {
     const value = await this.redisClient.get(key);
-    console.log(value);
     return value ? JSON.parse(value) : null;
   }
+
   async removeItem(key: string) {
-    const removests = await this.redisClient.del(key);
-    return removests;
+    return await this.redisClient.del(key);
   }
 }
